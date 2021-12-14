5207356cb91c18d297b2caeeb035da0e1a40f966
kyle <sdfcvbjkl@gmail.com>
2021-12-14 20:44:13 +0800

    feat: add 1.remove single/all order function 2.change order state

--- all.js
+++ all.js
@@ -3,17 +3,22 @@ const apiUrl = 'https://livejs-api.hexschool.io';
 const apiPath = 'kn99';
 const apiKey = 'HKqgwrl3jRP0mmi2Ds2IGaiANC33';
 
+const BYCATEGORY = 'byCategory';
+const BYITEM = 'byItem';
+
 const topMenu = document.querySelector('.topBar-menu');
-const ordersByCategory = document.querySelector('.ordersByCategory');
-const ordersByItem = document.querySelector('.ordersByItem');
 const orderTable = document.querySelector('.orderPage-table');
 const chartTitle = document.querySelector('.section-title');
+const removeAllOrder = document.querySelector('.discardAllBtn');
 
 let currentOrderData = {};
+let currentChartType = '';
 
 function init() {
     getOrderData();
     bindChartEvent();
+    bindRemoveOrderEvent();
+    bindChangePaidEvent();
 }
 
 init();
@@ -29,7 +34,7 @@ function getOrderData() {
     .then(res => {
         console.log(res.data);
         currentOrderData = res.data;
-        createOrdersByCategoryChart(res.data);
+        createChart(BYCATEGORY,res.data);
         renderOrderTable(res.data);
     })
     .catch(err => {
@@ -47,8 +52,20 @@ function bindChartEvent() {
     })
 }
 
+function createChart(type, orderData) {
+    switch(type){
+        case BYCATEGORY:
+            createOrdersByCategoryChart(orderData);
+            break;
+        case BYITEM:
+            createOrdersByItemChart(orderData);
+            break;
+    }
+}
+
 function createOrdersByCategoryChart(orderData) {
     chartTitle.textContent = '全產品類別營收比重';
+    currentChartType = BYCATEGORY;
 
     let categoryPrice = {}
     // 整理類別資料
@@ -86,6 +103,7 @@ function createOrdersByCategoryChart(orderData) {
 
 function createOrdersByItemChart(orderData) {
     chartTitle.textContent = '全品項營收比重';
+    currentChartType = BYITEM;
 
     let itemPrice = {};
     // 整理品項資料
@@ -137,32 +155,33 @@ function renderOrderTable(orderData) {
         </thead>
     `;
 
-    orders.forEach(order => {
-        str += `
-        <tr>
-            <td>${order.id}</td>
-            <td>
-            <p>${order.user.name}</p>
-            <p>${order.user.tel}</p>
-            </td>
-            <td>${order.user.address}</td>
-            <td>${order.user.email}</td>
-            <td>
-            <p>${order.products[0].title}</p>
-            </td>
-            <td>${getFormatDate(order.createdAt)}</td>
-            <td class="orderStatus">
-            <a href="#">${isPaid(order.paid)}</a>
-            </td>
-            <td>
-            <input type="button" class="delSingleOrder-Btn" value="刪除">
-            </td>
-        </tr>
-        `;
+    if (orders.length > 0) {
+        orders.forEach(order => {
+            str += `
+            <tr>
+                <td>${order.id}</td>
+                <td>
+                <p>${order.user.name}</p>
+                <p>${order.user.tel}</p>
+                </td>
+                <td>${order.user.address}</td>
+                <td>${order.user.email}</td>
+                <td>
+                <p>${order.products[0].title}</p>
+                </td>
+                <td>${getFormatDate(order.createdAt)}</td>
+                <td class="orderStatus">
+                <a href="#" data-id=${order.id} data-ispaid=${order.paid}>${isPaid(order.paid)}</a>
+                </td>
+                <td>
+                <input type="button" class="delSingleOrder-Btn" data-id=${order.id} value="刪除">
+                </td>
+            </tr>
+            `;
 
-        orderTable.innerHTML = str;
-    });
-
+        });
+    }
+    orderTable.innerHTML = str;
 }
 
 function getFormatDate(timestamp) {
@@ -176,7 +195,95 @@ function isPaid(paid) {
 
 function bindRemoveOrderEvent() {
 
+    // remove single order
+    orderTable.addEventListener('click', e => {
+        // console.log(e.target);
+        let tar = e.target;
+        if (tar.classList.value.includes('delSingleOrder-Btn')) {
+            deleteSingleOrder(tar.dataset.id);
+        }
+    });
 
+    // remove all orders
+    removeAllOrder.addEventListener('click', e => {
+        deleteAllOrders();
+    })
+}
 
+function deleteSingleOrder(orderId) {
+    axios({
+        method: 'delete',
+        url: `${apiUrl}​/api/livejs/v1/admin/${apiPath}/orders/${orderId}`,
+        headers: {
+            'authorization':apiKey
+        }
+    })
+    .then(res => {
+        // console.log(res.data);
+        renderOrderTable(res.data);
+        createChart(currentChartType, res.data);
+    })
+    .catch(err => {
+        console.log(err.response);
+    });
+}
 
+function deleteAllOrders(){
+    axios({
+        method: 'delete',
+        url: `${apiUrl}​/api/livejs/v1/admin/${apiPath}/orders`,
+        headers: {
+            'authorization':apiKey
+        }
+    })
+    .then(res => {
+        // console.log(res.data);
+        renderOrderTable(res.data);
+        createChart(currentChartType, res.data);
+    })
+    .catch(err => {
+        console.log(err.response);
+    });
+}
+
+function bindChangePaidEvent() {
+    orderTable.addEventListener('click', e => {
+        let tar = e.target;
+        let orderId = tar.dataset.id;
+        let newState = changeIsPaidState(tar.dataset.ispaid);
+        console.log(orderId, newState);
+        modifyOrderState(orderId, newState);
+    });
+}
+
+function changeIsPaidState(isPaidStr) {
+    if (isPaidStr === 'false') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function modifyOrderState(orderId, isPaid) {
+    axios({
+        method: 'put',
+        url: `${apiUrl}​/api/livejs/v1/admin/${apiPath}/orders`,
+        data: {
+            "data": {
+              "id": orderId,
+              "paid": isPaid
+            }
+          },
+        headers: {
+            'authorization':apiKey
+        }
+    })
+    .then(res => {
+        console.log(res.data);
+        renderOrderTable(res.data);
+        createChart(currentChartType, res.data);
+    })
+    .catch(err => {
+        console.log(err.response);
+    })
 }
